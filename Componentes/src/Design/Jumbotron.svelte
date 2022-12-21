5e16fd04b6f29bce08352f170e0a84aa60e0fb6c
PaterDev <josepater_1130@hotmail.com>
2022-12-21 16:08:18 -0500

    1.5.9 Uso de Slots en componentes

---
+++ Componentes/src/Design/Jumbotron.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let nombre;
+</script>
+
+<div class="jumbotron mt-4">
+    <h1>{nombre}</h1>
+    <!-- Permite agregar más elementos cuando se llama al componente de la forma tradicional -->
+    <slot></slot>
+</div>
